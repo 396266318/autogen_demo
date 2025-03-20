830bbbbc8c34592b71b84b3ba2228042577ba53a
玄月 <huxin39@163.com>
2025-03-20 21:45:52 +0800

    Update llms.py

--- llms.py
+++ llms.py
@@ -3,7 +3,7 @@ from autogen_ext.models.openai import OpenAIChatCompletionClient
 model_client = OpenAIChatCompletionClient(
     model="deepseek-chat",
     base_url="https://api.deepseek.com",
-    api_key="sk-38391b6e2c59451ab98a0f2a6ccd1c83",
+    api_key="sk-38391b6",
     model_info={
         "vision": False,
         "function_calling": True,
